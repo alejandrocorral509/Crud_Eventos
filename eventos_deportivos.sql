3143cd45088fe03bc2de16a315d6301673b87099
alejandrocorral509 <alejandrocorral509@gmail.com>
2024-12-03 09:24:08 +0100

    archivos subidos

---
+++ eventos_deportivos.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 08-11-2024 a las 08:30:15
+-- Versión del servidor: 8.3.0
+-- Versión de PHP: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `eventos_deportivos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+DROP TABLE IF EXISTS `eventos`;
+CREATE TABLE IF NOT EXISTS `eventos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_evento` varchar(255) NOT NULL,
+  `tipo_deporte` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `id_organizador` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_organizador` (`id_organizador`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `organizadores`
+--
+
+DROP TABLE IF EXISTS `organizadores`;
+CREATE TABLE IF NOT EXISTS `organizadores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
